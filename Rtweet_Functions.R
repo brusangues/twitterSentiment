42af3a2d1b2cd32f7543f6f5a92c5b0f1afe203a
Bruno S Rodrigues <34436334+brusangues@users.noreply.github.com>
2021-09-23 19:58:23 -0300

    Update

--- Rtweet_Functions.R
+++ Rtweet_Functions.R
@@ -12,11 +12,11 @@
 token <- function(x) {
   
   #Parametros iniciais
-  appname <- c("Mining Demo First", "Mining Demo Second")
-  consumer_key <- c("mk64zKzYZX80ETCz5AeYxepqW", "14B13oi1kRzQxD1CSuP147DaT")
-  consumer_secret <- c("a993kwnC15CizKmDx1ymJEZExWdGqoILBoHYedrbwqTuiVMnVE", "LE4cLIsOTF8BTSLGxowCnA0X90HhJNp5DdPA0n4tNhlZvkIsjQ")
-  access_token <- c("417475374-ApAzScCMc8AfUO0rgrwzy0eu3pjayhrzAnLP1qZ2", "1041079280770310144-y8KSsot5v4uIZmPkplFQu1mdZtgTSB")
-  access_secret <- c("LVvsp3hDksH6Gy5eGT6h4FLSwTjgW7x8RAdJzgFkCQSD5", "DDL6J0V5iF6w1qmSP5VvvMhV46NX28zmlm5u63H1xrfJ6")
+  appname         <- c("Mining Demo First", "Mining Demo Second")
+  consumer_key    <- c("-------------------------", "-------------------------")
+  consumer_secret <- c("--------------------------------------------------", "--------------------------------------------------")
+  access_token    <- c("--------------------------------------------------", "--------------------------------------------------")
+  access_secret   <- c("---------------------------------------------", "---------------------------------------------")
   
   return(
     create_token(
